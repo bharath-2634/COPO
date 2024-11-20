de46ed765411fa54afc668ff2fa4127a062152fe
Bharath <bharath2005goo@gmail.com>
2024-11-20 19:06:45 +0530

    fourth commit

--- frontend/src/pages/Attainment/DataEntry.jsx
+++ frontend/src/pages/Attainment/DataEntry.jsx
@@ -12,39 +12,8 @@ import ExcelJS from "exceljs";
 import { saveAs } from "file-saver";
 
 const DataEntry = () => {
-<<<<<<< HEAD
   
 
-=======
-  const [selectedCO, setSelectedCO] = useState("");
-  const [marks, setMarks] = useState("");
-  const [percentage, setPercentage] = useState("");
-  const [checkboxes, setCheckboxes] = useState({
-    L1: "",
-    L2: "",
-    L3: "",
-    L4: "",
-    L5: "",
-    L6: "",
-  });
-
-  const handleCheckboxChange = (level) => (e) => {
-    if (!e.target.checked) {
-      setCheckboxes((prev) => ({ ...prev, [level]: "" }));
-    }
-  };
-
-  const handleMarkChange = (level) => (e) => {
-    setCheckboxes((prev) => ({ ...prev, [level]: e.target.value }));
-  };
-
-  const validatedata = () => {
-    console.log(selectedCO);
-    console.log(marks);
-    console.log(percentage);
-    console.log(checkboxes);
-  };
->>>>>>> 1487de4e36da17bd69cf08f98102955f8daf6129
   const [allBatches, setAllBatches] = useState([]);
   const [batchId, setBatchId] = useState("");
 
@@ -73,19 +42,12 @@ const DataEntry = () => {
       const token = localStorage.getItem("token");
       console.log("token from client Side : " + token);
       console.log("batchId from client Side : " + batchId);
-<<<<<<< HEAD
     
-=======
->>>>>>> 1487de4e36da17bd69cf08f98102955f8daf6129
       const response = await axiosInstance.get("/get-allSubjects/" + batchId, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
-<<<<<<< HEAD
-=======
-
->>>>>>> 1487de4e36da17bd69cf08f98102955f8daf6129
       console.log(
         "Batch Data client side :",
         JSON.stringify(allBatch, null, 2)
@@ -104,7 +66,6 @@ const DataEntry = () => {
       console.log("An Unexpected Error from client side : " + error);
     }
   };
-<<<<<<< HEAD
 
   const [subjects, setAllSubjects] = useState([]);
   const [courseNumber, setCourseNumber] = useState([]);
@@ -267,20 +228,8 @@ const DataEntry = () => {
       },
     }));
   };
-=======
 
-  const [subjects, setAllSubjects] = useState([]);
-  const [courseNumber, setCourseNumber] = useState([]);
->>>>>>> 1487de4e36da17bd69cf08f98102955f8daf6129
 
-  const getSubjects = () => {
-    const names = allBatch.map((subject) => subject.courseName);
-    const cono = allBatch.map((subject) => subject.coNo);
-    setCourseNumber(cono);
-    setAllSubjects(names);
-  };
-
-<<<<<<< HEAD
   const generateSheet = (e) => {
     e.preventDefault();
     const mainHeader = ["S.NO", "REGISTER NUMBER", "STUDENT"];
@@ -344,60 +293,17 @@ const DataEntry = () => {
       // row.getCell(6).value = { formula: `ROUND(H${rowIndex + 1} * ${levelMarks.CO1.L2 / maxCO1}, 0)` };
       // row.getCell(7).value = { formula: `ROUND(H${rowIndex + 1} * ${levelMarks.CO1.L3 / maxCO1}, 0)` };
       // row.getCell(8).value = { formula: `H${rowIndex + 1} - (D${rowIndex + 1} + E${rowIndex + 1} + F${rowIndex + 1})` };
-=======
-  useEffect(() => {
-    getAllSubjects();
-    getSubjects();
-  }, [batchId]);
 
-  const [Iat, setIat] = useState(["IAT 1", "IAT 2", "IAT 3"]);
-  const [selectIat, setSelectIat] = useState("");
->>>>>>> 1487de4e36da17bd69cf08f98102955f8daf6129
+    }
 
-  const [openUpload, setOpenUpload] = useState(false);
 
-<<<<<<< HEAD
-=======
-  const generateExcel = () => {
-    const data = [
-      ["Name", "Age", "Score", "Total"],
-      ["John", 25, 50, null],
-      ["Alice", 30, 80, null],
-      ["Bob", 28, 70, null],
-    ];
 
-    const worksheet = XLSX.utils.aoa_to_sheet(data);
 
-    worksheet["D2"] = "C2+20";
-    worksheet["D3"] = "C3+20";
-    worksheet["D4"] = "C4+20";
-
-    const workbook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
->>>>>>> 1487de4e36da17bd69cf08f98102955f8daf6129
-
-    workbook.Workbook = {
-      CalcPr: {
-        fullCalcOnLoad: true,
-      },
-    };
-
-    XLSX.writeFile(workbook, "GeneratedExcel.xlsx");
-  };
-
-<<<<<<< HEAD
     setgeneratemodel(false);
 
   }
 
   
-=======
-  const handleUpload = () => {
-    setOpenUpload(false);
-  };
-
-  const [generatemodel, setgeneratemodel] = useState(false);
->>>>>>> 1487de4e36da17bd69cf08f98102955f8daf6129
 
   return (
     <>
@@ -426,10 +332,7 @@ const DataEntry = () => {
             onChange={(e) => {
               setSubjectId(e.target.value);
             }}
-<<<<<<< HEAD
 
-=======
->>>>>>> 1487de4e36da17bd69cf08f98102955f8daf6129
           >
             {subjects.map((subject, idx) => {
               return <option value={idx}>{subject}</option>;
@@ -513,11 +416,7 @@ const DataEntry = () => {
         </button>
       </div>
       <div>
-<<<<<<< HEAD
         { generatemodel && (
-=======
-        {generatemodel && (
->>>>>>> 1487de4e36da17bd69cf08f98102955f8daf6129
           <Modal
             isOpen={generatemodel}
             onRequestClose={() => setgeneratemodel(false)}
@@ -527,29 +426,17 @@ const DataEntry = () => {
                 backgroundColor: "rgba(0,0,0,.2)",
               },
             }}
-<<<<<<< HEAD
             className="w-[65%] max-h-2/4 bg-white rounded-md mx-auto mt-1 p-5"
-=======
-            className="w-[30%] max-h-2/4 bg-white rounded-md mx-auto mt-40 p-5"
->>>>>>> 1487de4e36da17bd69cf08f98102955f8daf6129
           >
             <div className="flex flex-col p-1 gap-3">
               <div className="w-full flex items-center justify-between">
                 <h2>Enter Data</h2>
-<<<<<<< HEAD
                 <button className="w-10 h-10 rounded-full flex items-center justify-center -mt-2 hover:bg-slate-50" onClick={() => setgeneratemodel(false)}>
-=======
-                <button
-                  className="w-10 h-10 rounded-full flex items-center justify-center -mt-2 hover:bg-slate-50"
-                  onClick={() => setgeneratemodel(false)}
-                >
->>>>>>> 1487de4e36da17bd69cf08f98102955f8daf6129
                   <MdClose className="text-xl text-slate-400" />
                 </button>
               </div>
 
               <form className="mt-2">
-<<<<<<< HEAD
                  {/* Input Form Box */}
                 <div className="flex flex-col w-full items-center justify-center gap-4">
                   <div className="flex items-center justify-between w-full gap-2">
@@ -706,176 +593,6 @@ const DataEntry = () => {
                 <div className="mt-6 w-full flex gap-16 items-center justify-around rounded text-white">
                   <button className="bg-green-500 p-2 rounded" onClick={(e)=>{generateSheet(e)}}>
                     <h2>Generate Sheet</h2>
-=======
-                <div className="flex flex-col gap-1 mt-3">
-                  <h3 className="text-sm text-slate-400 font-medium">CO.NO</h3>
-                  <select
-                    className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm/6"
-                    value={selectedCO}
-                    onChange={(e) => setSelectedCO(e.target.value)}
-                  >
-                    <option value="">Select CO</option>
-                    <option value="CO1">CO1</option>
-                    <option value="CO2">CO2</option>
-                    <option value="CO3">CO3</option>
-                    <option value="CO4">CO4</option>
-                    <option value="CO5">CO5</option>
-                    <option value="CO6">CO6</option>
-                  </select>
-                </div>
-
-                <div className="flex flex-col gap-1 mt-3">
-                  <h3 className="text-sm text-slate-400 font-medium">Marks</h3>
-                  <input
-                    type="number"
-                    className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm/6"
-                    value={marks}
-                    onChange={(e) => setMarks(e.target.value)}
-                    placeholder="Enter marks"
-                  />
-                </div>
-
-                <div className="flex flex-col gap-1 mt-3">
-                  <h3 className="text-sm text-slate-400 font-medium">
-                    Percentage
-                  </h3>
-                  <input
-                    type="number"
-                    className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm/6"
-                    value={percentage}
-                    onChange={(e) => setPercentage(e.target.value)}
-                    placeholder="Enter percentage"
-                  />
-                </div>
-
-                <div className="flex flex-wrap gap-4 mt-3">
-                  <div className="flex items-center gap-3">
-                    <input
-                      type="checkbox"
-                      id="L1"
-                      checked={checkboxes.L1}
-                      onChange={handleCheckboxChange("L1")}
-                    />
-                    <label htmlFor="L1" className="text-sm text-slate-400">
-                      L1
-                    </label>
-                    <input
-                      type="text"
-                      value={checkboxes.L1}
-                      onChange={handleMarkChange("L1")}
-                      className="w-16 rounded-md border-0 py-1 px-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm/6"
-                      placeholder="Mark"
-                    />
-                  </div>
-
-                  <div className="flex items-center gap-3">
-                    <input
-                      type="checkbox"
-                      id="L2"
-                      checked={checkboxes.L2}
-                      onChange={handleCheckboxChange("L2")}
-                    />
-                    <label htmlFor="L2" className="text-sm text-slate-400">
-                      L2
-                    </label>
-                    <input
-                      type="text"
-                      value={checkboxes.L2}
-                      onChange={handleMarkChange("L2")}
-                      className="w-16 rounded-md border-0 py-1 px-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm/6"
-                      placeholder="Mark"
-                    />
-                  </div>
-
-                  <div className="flex items-center gap-3">
-                    <input
-                      type="checkbox"
-                      id="L3"
-                      checked={checkboxes.L3}
-                      onChange={handleCheckboxChange("L3")}
-                    />
-                    <label htmlFor="L3" className="text-sm text-slate-400">
-                      L3
-                    </label>
-                    <input
-                      type="text"
-                      value={checkboxes.L3}
-                      onChange={handleMarkChange("L3")}
-                      className="w-16 rounded-md border-0 py-1 px-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm/6"
-                      placeholder="Mark"
-                    />
-                  </div>
-
-                  <div className="flex items-center gap-3">
-                    <input
-                      type="checkbox"
-                      id="L4"
-                      checked={checkboxes.L4}
-                      onChange={handleCheckboxChange("L4")}
-                    />
-                    <label htmlFor="L4" className="text-sm text-slate-400">
-                      L4
-                    </label>
-                    <input
-                      type="text"
-                      value={checkboxes.L4}
-                      onChange={handleMarkChange("L4")}
-                      className="w-16 rounded-md border-0 py-1 px-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm/6"
-                      placeholder="Mark"
-                    />
-                  </div>
-
-                  <div className="flex items-center gap-3">
-                    <input
-                      type="checkbox"
-                      id="L5"
-                      checked={checkboxes.L5}
-                      onChange={handleCheckboxChange("L5")}
-                    />
-                    <label htmlFor="L5" className="text-sm text-slate-400">
-                      L5
-                    </label>
-                    <input
-                      type="text"
-                      value={checkboxes.L5}
-                      onChange={handleMarkChange("L5")}
-                      className="w-16 rounded-md border-0 py-1 px-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm/6"
-                      placeholder="Mark"
-                    />
-                  </div>
-
-                  <div className="flex items-center gap-3">
-                    <input
-                      type="checkbox"
-                      id="L6"
-                      checked={checkboxes.L6}
-                      onChange={handleCheckboxChange("L6")}
-                    />
-                    <label htmlFor="L6" className="text-sm text-slate-400">
-                      L6
-                    </label>
-                    <input
-                      type="text"
-                      value={checkboxes.L6}
-                      onChange={handleMarkChange("L6")}
-                      className="w-16 rounded-md border-0 py-1 px-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm/6"
-                      placeholder="Mark"
-                    />
-                  </div>
-                </div>
-
-                <div className="mt-6 w-full flex gap-16 items-center justify-around rounded text-white">
-                  <button
-                    className="bg-green-500 p-2 rounded"
-                    onClick={() => {
-                      validatedata();
-                    }}
-                  >
-                    Submit
-                  </button>
-                  <button className="bg-green-500 py-2 px-4 rounded-2xl">
-                    +
->>>>>>> 1487de4e36da17bd69cf08f98102955f8daf6129
                   </button>
                 </div>
               </form>
